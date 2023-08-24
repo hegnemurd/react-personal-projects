82102e45b2f1a322dfcce988f7dc7f196c79036b
hegnemurd <hegne.murd@gmail.com>
2023-08-24 19:58:11 +0100

    Following a tutorial for making a todo list using React

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -15,9 +15,22 @@ const mapDispathToProps = (dispatch) => {
 };
 
 const Todos = (props) => {
-  console.log("props", props);
+  // console.log("props", props);
 
   const [todo, setTodo] = useState("");
+
+  const add = () => {
+    if (todo === "") {
+      alert("Input is Empty");
+    } else {
+      props.addTodo({
+        id: Math.floor(Math.random() * 1000),
+        item: todo,
+        completed: false,
+      });
+      setTodo("");
+    }
+  };
 
   const handleChange = (e) => {
     setTodo(e.target.value);
@@ -32,15 +45,10 @@ const Todos = (props) => {
         value={todo}
       />
 
-      <button className="add-btn">Add</button>
+      <button className="add-btn" onClick={() => add()}>
+        Add
+      </button>
       <br />
-
-      <ul>
-        {props.todos.length > 0 &&
-          props.todos.map((item) => {
-            return <li key={item.id}>{item.item}</li>;
-          })}
-      </ul>
     </div>
   );
 };
